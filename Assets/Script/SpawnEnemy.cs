4101cc7f941d3ba3cb282c83742631f1f59089ea
Douglas Araújo <azarujo100@gmail.com>
2023-01-11 01:10:32 -0300

    Add enemy
    Adição do inimigo e criação do spawn

---
+++ Assets/Script/SpawnEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemysGame()
+    {
+
+    }
+}
